1cbc031850581c5eec92420265f9899b7a73d4a3
Guinslym <guinslym@gmail.com>
2017-08-05 14:41:01 -0400

    react config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+	, document.getElementById('root')
+);
 registerServiceWorker();
